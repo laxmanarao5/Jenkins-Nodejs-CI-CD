b6ded753c2fe9f9d6f9d45aa2794ec142a6e7f25
laxman <laxmanarao.b@westagilelabs.com>
2023-07-04 13:16:47 +0530

    Dev branch added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/test",(req,res)=>{
     res.send({message:"Test API"})
 })
 
+app.get("/dev",(req,res)=>{
+    res.send({message:"API added from dev branch"})
+})
+
 app.listen(80,()=>{
     console.log("Server started at 80");
 })
